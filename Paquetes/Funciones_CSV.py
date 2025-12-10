ec2eb70e4009b299efbd2c5013b67a2b68b42025
Luca Manuel Velozo <lucavelozo@live.com>
2025-12-10 14:53:02 -0300

    ADD RANKING Y CORRECCIONES

--- Paquetes/Funciones_CSV.py
+++ Paquetes/Funciones_CSV.py
@@ -1,8 +1,10 @@
 import csv
+import os
 import Paquetes.Funciones_Juego as Fj
 
 categorias_CSV = {
-    "nombre:":None,
+    "nombre:": None,
+    "nivel:": None,
     "1": None,
     "2": None,
     "3": None,
@@ -23,14 +25,19 @@ def cargar_csv():
         for cate in categorias_CSV:
             escritor.writerow([cate, None])
         
-def cargar_nombre():
+def cargar_nombre_y_nivel(nivel):
     with open("puntos.csv", "r", newline="") as archivo:
         lector = csv.DictReader(archivo)
         filas = list(lector)
     nombre = input("Ingrese su nombre: ").strip()
+    while nombre == "":
+        print("No puede dejar el campo vacío")
+        nombre = input("Ingrese su nombre: ").strip()
     for fila in filas:
         if fila["categoria"] == "nombre:":
-            fila["puntos"] = nombre    
+            fila["puntos"] = nombre
+        if fila["categoria"] == "nivel:":
+            fila["puntos"] = nivel  
 
     with open("puntos.csv", "w", newline="") as archivo:
         campos = ["categoria", "puntos"]
@@ -66,6 +73,8 @@ def total_puntos_csv():
     total = 0
 
     for fila in filas:
+        if fila["categoria"] == "total":
+            continue
         puntos = fila["puntos"]
         if puntos is None or puntos == "" or not puntos.isdigit():
             continue
@@ -114,3 +123,75 @@ def sumar_puntos(categoria_elegida, puntos):
         escritor.writerows(filas)
 
 
+def puntos_ranking(jugador):
+        return int(jugador["total"])
+
+def act_ranking():
+    nombre = None
+    total = None
+
+    with open("puntos.csv", "r", newline="") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            if fila["categoria"] == "nombre:":
+                nombre = fila["puntos"]
+            elif fila["categoria"] == "total":
+                total = fila["puntos"]
+
+    if nombre is None or total is None:
+        print("Error: nombre o total no encontrados en puntos.csv")
+        return
+
+    total = int(total) 
+    ranking = []
+    jugador_existente = False
+
+    if os.path.exists("Ranking.csv"):
+        with open("Ranking.csv", "r", newline="") as archivo:
+            lector = csv.DictReader(archivo)
+            for fila in lector:
+                fila_total = int(fila["total"])
+
+                if fila["nombre"] == nombre:
+                    jugador_existente = True
+                    fila_total = max(fila_total, total)
+
+                ranking.append({
+                    "nombre": fila["nombre"],
+                    "total": fila_total
+                })
+
+    if not jugador_existente:
+        ranking.append({"nombre": nombre, "total": total})
+
+    ranking.sort(key=puntos_ranking,reverse=True)
+
+    with open("Ranking.csv", "w", newline="") as archivo:
+        campos = ["nombre", "total"]
+        escritor = csv.DictWriter(archivo, fieldnames=campos)
+        escritor.writeheader()
+        escritor.writerows(ranking)
+
+    print("Ranking actualizado correctamente.")
+
+def leer_ranking():
+    ranking = []
+    contador = 0
+    with open("Ranking.csv", "r", newline="") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            ranking.append(fila)
+    
+    ranking.sort(key=puntos_ranking,reverse=True)
+    
+    if not ranking:
+        print("Ranking vacío, jugá para agregar entradas!")
+    
+    else:
+        print("======== TOP 10 ========")
+        for i in ranking:
+            if contador == 10:
+                break
+            print(i)
+            contador += 1
+    input("=== PRESIONE ENTER PARA VOLVER AL MENÚ PRINCIPAL ===")
--- Paquetes/Funciones_Juego.py
+++ Paquetes/Funciones_Juego.py
@@ -6,7 +6,7 @@ import Paquetes.Funciones_json as Fjson
 
 def jugar(nivel_seleccionado):
     Fcsv.cargar_csv()
-    nombre_archivo = Fcsv.cargar_nombre()
+    Fcsv.cargar_nombre_y_nivel(nivel_seleccionado["nombre"])
     ronda = 0
     
     while ronda != 10:
@@ -25,12 +25,11 @@ def jugar(nivel_seleccionado):
             dados_usables , dados_mano, lista_dados = guardar_dados(lista_dados, dados_usables, dados_mano)
             dados_def = lista_dados + dados_mano
             tiradas = opciones_ronda(tiradas, dados_def, nivel_seleccionado)
-            print(dados_def)
             print(f"\n=== FIN TIRADA N°{tiradas} ===")
 
         print(f"\n=== FIN RONDA N°{ronda} ===")
     Fjson.guardar_json_partida()
-    Fjson.guardar_datos()
+    Fcsv.act_ranking()
    
             
 def tirar_dados(dados_usables):
@@ -82,18 +81,17 @@ def guardar_dados(lista_dados, dados_usables, dados_mano):
                                 print(f"Error: La posición {pos} ya fue elegida.")
                                 error_encontrado = True
                                 break
-                        else:
 
+                        else:
                             print(f"Error: La posición {pos} no existe.")
                             error_encontrado = True
                             break
+
                     else:
-                        
                         print(f"Error: '{parte}' no es un número válido.")
                         error_encontrado = True
                         break
-                
-                
+
                 if not error_encontrado and len(indices_temporales) > 0:
                     indices_a_borrar = indices_temporales
                     entrada_valida = True
@@ -122,38 +120,45 @@ def guardar_dados(lista_dados, dados_usables, dados_mano):
     return dados_usables, dados_mano, lista_dados
 
 def opciones_ronda(tiradas, dados_def, nivel_seleccionado):
-    if tiradas == 0:
-        print('''
-        =======================================
-          eliga una opción:
-          1-TIRAR
-        =======================================''')
-    elif tiradas < 3 and tiradas >=1:
+    if tiradas < 3 and tiradas >=1:
         print('''
             =======================================
              eliga una opción:
               1-anotar puntos
               2-siguiente tirada
             =======================================''')
+        exit = False
+        while exit == False:
+            opciones = ""    
+            while opciones != "1" or opciones != "2":
+                opciones = str(input("==="))
+                if opciones == "1":
+                    Fc.comparacion_mano(dados_def, nivel_seleccionado)
+                    tiradas = 3
+                    exit = True
+                    return tiradas
+                elif opciones == "2":
+                    exit = True
+                    return tiradas
+                else:
+                    print("ingrese un valor válido")
     else:
         print('''
         =======================================
           eliga una opción:
           1-anotar puntos
         =======================================''')
-    opciones = int(0)
-    exit = False
-    while exit == False:
-        opciones = int(input("==="))
-        if opciones == 1:
-            Fc.comparacion_mano(dados_def, nivel_seleccionado)
-            tiradas = 3
-            exit = True
-            return tiradas
-        elif opciones == 2:
-            exit = True
-            return tiradas
-        else:
-            print("ingrese un valor válido")
+        exit = False
+        while exit == False:
+            opciones = ""    
+            while opciones != "1":
+                opciones = str(input("==="))
+                if opciones == "1":
+                    Fc.comparacion_mano(dados_def, nivel_seleccionado)
+                    tiradas = 3
+                    exit = True
+                    return tiradas
+                else:
+                    print("ingrese un valor válido")
 
 
