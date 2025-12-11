f3ea4db40dbc66590af3d9be90f26c3e21266e63
Luca Manuel Velozo <lucavelozo@live.com>
2025-12-11 09:38:22 -0300

    Update Funciones_Juego.py

--- Paquetes/Funciones_Juego.py
+++ Paquetes/Funciones_Juego.py
@@ -4,9 +4,18 @@ import Paquetes.Funciones_CSV as Fcsv
 import Paquetes.Funciones_json as Fjson
 
 
+runas = {
+    1: "⟡ Althar",
+    2: "ᚢ Vekir",
+    3: "ᚦ Tryna",
+    4: "ᚨ Kaldur",
+    5: "ᚱ Sereth",
+    6: "ᚹ Zhal"
+}
+#
 def jugar(nivel_seleccionado):
     Fcsv.cargar_csv()
-    nombre_archivo = Fcsv.cargar_nombre()
+    Fcsv.cargar_nombre_y_nivel(nivel_seleccionado["nombre"])
     ronda = 0
     
     while ronda != 10:
@@ -17,33 +26,39 @@ def jugar(nivel_seleccionado):
         dados_def=[]
         while tiradas != 3:
             tiradas += 1
-            print(f"RONDA N°{ronda}")
+            print(f"\nRONDA N°{ronda}")
             print(f"tirada numero |{tiradas}|")
             print(f"PUNTOS |{Fcsv.total_puntos_csv()}|")
+            planilla_puntajes()
             print(f"dados en mano |{dados_mano}|")
             lista_dados = tirar_dados(dados_usables)
             dados_usables , dados_mano, lista_dados = guardar_dados(lista_dados, dados_usables, dados_mano)
             dados_def = lista_dados + dados_mano
             tiradas = opciones_ronda(tiradas, dados_def, nivel_seleccionado)
-            print(dados_def)
             print(f"\n=== FIN TIRADA N°{tiradas} ===")
 
         print(f"\n=== FIN RONDA N°{ronda} ===")
     Fjson.guardar_json_partida()
-    Fjson.guardar_datos()
-   
-            
+    Fcsv.act_ranking()
+              
 def tirar_dados(dados_usables):
     lista_dados=[]
     for i in range (dados_usables):
         lista_dados.append(random.randint(1, 6))
     print("\n=== DADOS ===")
-    print(lista_dados)
+    if dados_usables == 5:
+        print(f"===  {lista_dados[0]}   =====   {lista_dados[1]}   =====   {lista_dados[2]}   =====   {lista_dados[3]}   =====   {lista_dados[4]}   ===",)
+    elif dados_usables == 4:
+        print(f"===  {lista_dados[0]}   =====   {lista_dados[1]}   =====   {lista_dados[2]}   =====   {lista_dados[3]}   ===",)
+    elif dados_usables == 3:
+        print(f"===  {lista_dados[0]}   =====   {lista_dados[1]}   =====   {lista_dados[2]}   ===",)
+    elif dados_usables == 2:
+        print(f"===  {lista_dados[0]}   =====   {lista_dados[1]}   ===",)
+    elif dados_usables == 1:
+        print(f"===  {lista_dados[0]}   ===",)
+    print([runas[dado] for dado in lista_dados])
     print("\n=============")
     return(lista_dados)
-
-
-
 
 def guardar_dados(lista_dados, dados_usables, dados_mano):
     desea = ""
@@ -82,18 +97,17 @@ def guardar_dados(lista_dados, dados_usables, dados_mano):
                                 print(f"Error: La posición {pos} ya fue elegida.")
                                 error_encontrado = True
                                 break
-                        else:
 
+                        else:
                             print(f"Error: La posición {pos} no existe.")
                             error_encontrado = True
                             break
+
                     else:
-                        
                         print(f"Error: '{parte}' no es un número válido.")
                         error_encontrado = True
                         break
-                
-                
+
                 if not error_encontrado and len(indices_temporales) > 0:
                     indices_a_borrar = indices_temporales
                     entrada_valida = True
@@ -122,41 +136,52 @@ def guardar_dados(lista_dados, dados_usables, dados_mano):
     return dados_usables, dados_mano, lista_dados
 
 def opciones_ronda(tiradas, dados_def, nivel_seleccionado):
-    if tiradas == 0:
-        print('''
-        =======================================
-          eliga una opción:
-          1-TIRAR
-        =======================================''')
-    elif tiradas < 3 and tiradas >=1:
+    if tiradas < 3 and tiradas >=1:
         print('''
             =======================================
              eliga una opción:
               1-anotar puntos
               2-siguiente tirada
             =======================================''')
+        exit = False
+        while exit == False:
+            opciones = ""    
+            while opciones != "1" or opciones != "2":
+                opciones = str(input("==="))
+                if opciones == "1":
+                    Fc.comparacion_mano(dados_def, nivel_seleccionado)
+                    tiradas = 3
+                    exit = True
+                    return tiradas
+                elif opciones == "2":
+                    exit = True
+                    return tiradas
+                else:
+                    print("ingrese un valor válido")
     else:
         print('''
         =======================================
           eliga una opción:
           1-anotar puntos
         =======================================''')
-    opciones = int(0)
-    exit = False
-    while exit == False:
-        opciones = int(input("==="))
-        if opciones == 1:
-            print("===tus dados:===")
-            print(dados_def)
-            print("===jugadas posibles===")
-            Fc.comparacion_mano(dados_def, nivel_seleccionado)
-            tiradas = 3
-            exit = True
-            return tiradas
-        elif opciones == 2:
-            exit = True
-            return tiradas
-        else:
-            print("ingrese un valor válido")
+        exit = False
+        while exit == False:
+            opciones = ""    
+            while opciones != "1":
+                opciones = str(input("==="))
+                if opciones == "1":
+                    Fc.comparacion_mano(dados_def, nivel_seleccionado)
+                    tiradas = 3
+                    exit = True
+                    return tiradas
+                else:
+                    print("ingrese un valor válido")
+
+def planilla_puntajes():
+    planilla = Fcsv.csv_a_diccionario()
+    for clave, valor in planilla.items():
+        print(f"== {clave}:    {valor}")
+      
+
 
 
